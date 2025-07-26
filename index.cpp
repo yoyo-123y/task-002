7ca1886c7b090f1e818e56adcb4ec7ec996b9750
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 12:36:28 +0300

    complete it and test it

--- index.cpp
+++ index.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     int arr1[3];
     int arr2[3];
+    bool flag=true;
     cout<<"Enter the values for array 1 :"<<endl;
     for(int i = 0;i<3;i++){
         cout<<"Enter element number "<<(i+1)<<" = ";
@@ -17,8 +18,17 @@ int main(){
         cin>>arr2[i];
         cout<<endl;
     }
-    
-    cout << "Press Enter to exit...";
+    for(int i = 0;i<3;i++){
+        if(arr1[i] != arr2[i]){
+            flag=false;
+        }
+    }
+    if(flag){
+        cout<<"There are no differences";
+    }else{
+        cout<<"There are differences";
+    }
+    cout<< endl << "Press Enter to exit...";
     cin.ignore();
     cin.get();
 }
