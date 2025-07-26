8fe78083d4d1bda212e066697ec078a2239bc2fc
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 23:59:00 +0300

    prepared data entry

--- index.cpp
+++ index.cpp
@@ -3,5 +3,15 @@
 
 using namespace std;
 int main(){
+    int arr[10];
+    for(int i=0;i<10;i++){
+        cout<<"Enter the element number "<<(i+1)<<" = ";
+        cin>>arr[i];
+        cout<<endl;
+    }
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+        cout<<endl;
 
 }
