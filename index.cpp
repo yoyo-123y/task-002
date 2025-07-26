a89df5cb8bd33a79c87b55504cf0c2525c2e9268
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 19:06:24 +0300

    prepared the data entry

--- index.cpp
+++ index.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 int main(){
+    int arr1[6];
+    bool flag=true;
+    cout<<"Enter the values for array ordered :"<<endl;
+    for(int i = 0;i<6;i++){
+        cout<<"Enter element number "<<(i+1)<<" = ";
+        cin>>arr1[i];
+        cout<<endl;
+    }
+    for(int i = 0;i<6;i++){
+        cout<<arr1[i]<<" ";
+    }
+    cout<<endl;
     cout << "Press Enter to exit...";
     cin.ignore();
     cin.get();
