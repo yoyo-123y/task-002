58bc8ae61225355f752f0d81d9e5c32fbe9ce878
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 12:09:24 +0300

    written the code and tested it

--- index.cpp
+++ index.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 int main(){
+    string word;
+    cout<<"Enter the word = ";
+    cin>>word;
+    cout<<endl;
+    for(int i = word.length();i>=0;i--){
+        cout<<word[i];
+    }
+    cout<<endl;
     cout << "Press Enter to exit...";
     cin.ignore();
     cin.get();
