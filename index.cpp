77ac8bba297aed932d92a4c1814f0c5ed38f9fed
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 12:22:11 +0300

    written the data entry and tested it

--- index.cpp
+++ index.cpp
@@ -3,6 +3,26 @@
 
 using namespace std;
 int main(){
+    int arr1[3];
+    int arr2[3];
+    cout<<"Enter the values for array 1 :"<<endl;
+    for(int i = 0;i<3;i++){
+        cin>>arr1[i];
+        cout<<endl;
+    }
+    cout<<"Enter the values for array 2 :"<<endl;
+    for(int i = 0;i<3;i++){
+        cin>>arr2[i];
+        cout<<endl;
+    }
+    for(int i = 0;i<3;i++){
+        cout<<arr1[i]<<" ";
+    }
+    cout<<endl;
+    for(int i = 0;i<3;i++){
+        cout<<arr2[i]<<" ";
+    }
+    cout<<endl;
     cout << "Press Enter to exit...";
     cin.ignore();
     cin.get();
