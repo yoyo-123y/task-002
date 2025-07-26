a9e78d378424f012ec8d8dd90d273e2f0d9ac472
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 20:53:11 +0300

    tested the code

--- index.cpp
+++ index.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
-
+#include <limits>
 using namespace std;
 int main(){
     int arr1[6];
     int searchKey;
-    bool flag=true;
+    bool flag=false;
     cout<<"Enter the values for array ordered :"<<endl;
     for(int i = 0;i<6;i++){
         cout<<"Enter element number "<<(i+1)<<" = ";
@@ -15,7 +15,24 @@ int main(){
     cout<<"Enter the search key = ";
     cin>>searchKey;
     cout<<endl;
-    cout << "Press Enter to exit...";
-    cin.ignore();
-    cin.get();
+    int start=0;
+    int end=5;
+    int middle=(start+end)/2;
+    while(end>=start&&flag==false){
+        middle=(start+end)/2;
+        if(arr1[middle]==searchKey){
+            flag=true;
+        }else{
+            if(arr1[middle]>searchKey){
+                end=middle-1;
+            }else{
+                start=middle+1;
+            }
+        }
+    }
+    if(flag){
+        cout<<"found the value"<<endl;
+    }else{
+        cout<<"Didn't found the value"<<endl;
+    }
 }
