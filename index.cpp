005a3eb045ccb1e695070d7c647a876208210ac8
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 12:27:18 +0300

    edited data entry

--- index.cpp
+++ index.cpp
@@ -7,22 +7,17 @@ int main(){
     int arr2[3];
     cout<<"Enter the values for array 1 :"<<endl;
     for(int i = 0;i<3;i++){
+        cout<<"Enter element number "<<(i+1)<<" = ";
         cin>>arr1[i];
         cout<<endl;
     }
     cout<<"Enter the values for array 2 :"<<endl;
     for(int i = 0;i<3;i++){
+        cout<<"Enter element number "<<(i+1)<<" = ";
         cin>>arr2[i];
         cout<<endl;
     }
-    for(int i = 0;i<3;i++){
-        cout<<arr1[i]<<" ";
-    }
-    cout<<endl;
-    for(int i = 0;i<3;i++){
-        cout<<arr2[i]<<" ";
-    }
-    cout<<endl;
+    
     cout << "Press Enter to exit...";
     cin.ignore();
     cin.get();
