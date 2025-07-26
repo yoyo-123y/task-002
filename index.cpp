6a07cfb1faa80d144fc9a36792bcfbbacf9ee539
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 14:27:12 +0300

    written data entery and tested it

--- index.cpp
+++ index.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 int main(){
+    int carry,sum,binary1,binary2;
+    string sBin1,sBin2;
+    cout<<"Enter the first binary number = ";
+    cin>>binary1;
+    cout<<endl<<"Enter the second binary number = ";
+    cin>>binary2;
+    cout<<endl;
+    sBin1=to_string(binary1);
+    sBin2=to_string(binary2);
+    cout<<sBin1<<" "<<sBin2<<endl;
     cout << "Press Enter to exit...";
     cin.ignore();
     cin.get();
