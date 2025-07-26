0ada9fc1b7477978c3ac287e78e99d2649d0eb54
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 14:34:23 +0300

    made them equal in length

--- index.cpp
+++ index.cpp
@@ -12,6 +12,15 @@ int main(){
     cout<<endl;
     sBin1=to_string(binary1);
     sBin2=to_string(binary2);
+    if(sBin1.length()>sBin2.length()){
+        for(int i = sBin2.length();i<sBin1.length();i++){
+            sBin2.insert(0,"0");
+        }
+    }else{
+        for(int i = sBin1.length();i<sBin2.length();i++){
+            sBin1.insert(0,"0");
+        }
+    }
     cout<<sBin1<<" "<<sBin2<<endl;
     cout << "Press Enter to exit...";
     cin.ignore();
