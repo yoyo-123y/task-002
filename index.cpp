ae9181f7e858325bb2992b03cf6388bc3083c57c
youssif roushdy <youssifalsayad5656@gmail.com>
2025-07-26 21:26:46 +0300

    written the code and tested it

--- index.cpp
+++ index.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 using namespace std;
 int main(){
-
+    string word;
+    cout<<"Enter the word = ";
+    cin>>word;
+    cout<<endl;
+    sort(word.begin(),word.end());
+    do{
+        cout<<word<<endl;
+    }while(next_permutation(word.begin(),word.end()));
+    return 0;
 }
